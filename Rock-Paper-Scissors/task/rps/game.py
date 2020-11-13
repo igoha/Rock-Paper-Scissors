110501f50fb696c7c250f783f74a0a64c4083a34
igoha <igor_hajko@yahoo.com>
2020-11-13 01:04:22 +0100

    Initial commit

---
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -0,0 +1,8 @@
+user_choise = input()
+if user_choise == "rock":
+    choise = "paper"
+elif user_choise == "paper":
+    choise = "scissors"
+else:
+    choise = "rock"
+print(f"Sorry, but the computer chose {choise}")
