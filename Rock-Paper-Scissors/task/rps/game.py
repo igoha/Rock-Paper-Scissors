b48f47c3349ccd3b3cf9bb433597050c49a55053
igoha <igor_hajko@yahoo.com>
2020-11-23 20:42:08 +0100

    Stage 3

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,14 +1,19 @@
 import random
 
-
-lose_dict = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
-random.seed()
-random_word = random.choice(list(lose_dict.keys()))
-user_choise = input().lower()
-lose_word = lose_dict.get(random_word)
-if user_choise == random_word:
-    print(f"There is a draw {user_choise}")
-elif user_choise == lose_word:
-    print(f"Well done. The computer chose {random_word} and failed")
-else:
-    print(f"Sorry, but the computer chose {random_word}")
+while True:
+    lose_dict = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+    random.seed()
+    random_word = random.choice(list(lose_dict.keys()))
+    user_choise = input().lower()
+    if user_choise == "!exit":
+        break
+    elif user_choise not in list(lose_dict.keys()):
+        print("Invalid input")
+    else:
+        lose_word = lose_dict.get(random_word)
+        if user_choise == random_word:
+            print(f"There is a draw {user_choise}")
+        elif user_choise == lose_word:
+            print(f"Well done. The computer chose {random_word} and failed")
+        else:
+            print(f"Sorry, but the computer chose {random_word}")
